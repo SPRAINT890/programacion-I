b1452077b4e8c0315d2267137418b92d0b5bba81
unknown <gasparmorales1@gmail.com>
2022-09-14 18:31:33 -0300

    16 y 17 p4.2

--- Practico 4.2/ejercicio16_4.2.py
+++ Practico 4.2/ejercicio16_4.2.py
@@ -2,3 +2,27 @@
 Escriba un programa en Python para encontrar divisores comunes entre dos
 números en un par dado.
 """
+
+def get_divisores(mayor, menor):
+    c = 1
+    divisoresDelChico = [] 
+    divisores = []
+    while c <= menor:
+        if not menor % c:
+            divisoresDelChico.append(c)
+        c += 1
+    for numeros in divisoresDelChico:
+        if not mayor % numeros:
+            divisores.append(numeros)
+    return divisores
+
+
+
+numeros = [int(input("Ingrese un numero: ")), int(input("Ingrese otro numero: "))]
+
+if numeros[0] > numeros[1]:
+    print(get_divisores(numeros[0], numeros[1]))
+elif numeros[1] > numeros[0]:
+    print(get_divisores(numeros[1], numeros[0]))
+else:
+    print(get_divisores(numeros[0], numeros[1]))
---
+++ Practico 4.2/ejercicio17_4.2.py
@@ -0,0 +1,7 @@
+"""
+Escriba un programa Python que reciba una fecha e imprima el día de la fecha.
+Por ejemplo, si se recibe 02/08/2022, se debe imprimir 02 de Agosto del 2022.
+"""
+
+fecha = input("fecha: ")
+
