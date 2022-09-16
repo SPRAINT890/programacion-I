44929869aebb07d46ee25ea375ef43b93e05a03f
MORALES BURGUEÑO Gaspar <gmorales1@correo.um.edu.uy>
2022-09-15 22:41:09 -0300

    mejoras 18

--- Practico 4.2/ejercicio18_4.2.py
+++ Practico 4.2/ejercicio18_4.2.py
@@ -8,4 +8,25 @@ def fecha_a_tres_variables(fecha):
     mes = fecha[3:5]
     año = fecha[6:]
 
-    return dia, mes, año
+    return int(dia), int(mes), int(año)
+
+def diferencia_num(num1, num2):
+    if num1 < num2:
+        dif_num = num1 - num2
+    else:
+        dif_num = num2 - num1
+    return dif_num
+
+def mostrar_diferencia_entre_fechas(fecha1, fecha2):
+    dia1, mes1, año1 = fecha_a_tres_variables(fecha1)
+    dia2, mes2, año2 = fecha_a_tres_variables(fecha2)
+
+    dif_año = diferencia_num(año1, año2)
+    dif_mes = diferencia_num(mes1, mes2)
+    dif_dia = diferencia_num(dia1, dia2)
+    
+
+
+    
+
+mostrar_diferencia_entre_fechas(input("Fecha 1: "), input("Fecha 2: "))
