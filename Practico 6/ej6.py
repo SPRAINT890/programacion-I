3f0925f784485af63d94c44df95df1bd7a7b89e5
Gaspar Morales <gasparmorales1@gmail.com>
2022-10-07 21:31:56 -0300

    ej6 con letra

---
+++ Practico 6/ej6.py
@@ -0,0 +1,8 @@
+"""
+Escriba una clase que permita calcular el número PI con una precisión de "n"
+decimales. El parámetro "n" deberá introducirse por teclado.
+Puede utilizar la siguiente fórmula para aproximar el valor de PI/4:
+
+PI/4 = 1 - 1/3 + 1/5 - 1/7 +... = Suma(n=0,infinito){(-1)^n/(2n+1)}
+
+"""
